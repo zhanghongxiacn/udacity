4be44cf70401ee6d878f3c42c5e56ba7930855b3
zhanghongxiacn <zhanghongxia8869@yahoo.com>
2017-10-18 18:56:41 +0800

    Add files via upload

---
+++ process_osm.py
@@ -0,0 +1,430 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+After auditing is complete the next step is to prepare the data to be inserted into a SQL database.
+To do so you will parse the elements in the OSM XML file, transforming them from document format to
+tabular format, thus making it possible to write to .csv files.  These csv files can then easily be
+imported to a SQL database as tables.
+
+The process for this transformation is as follows:
+- Use iterparse to iteratively step through each top level element in the XML
+- Shape each element into several data structures using a custom function
+- Utilize a schema and validation library to ensure the transformed data is in the correct format
+- Write each data structure to the appropriate .csv files
+
+We've already provided the code needed to load the data, perform iterative parsing and write the
+output to csv files. Your task is to complete the shape_element function that will transform each
+element into the correct format. To make this process easier we've already defined a schema (see
+the schema.py file in the last code tab) for the .csv files and the eventual tables. Using the 
+cerberus library we can validate the output against this schema to ensure it is correct.
+
+## Shape Element Function
+The function should take as input an iterparse Element object and return a dictionary.
+
+### If the element top level tag is "node":
+The dictionary returned should have the format {"node": .., "node_tags": ...}
+
+The "node" field should hold a dictionary of the following top level node attributes:
+- id
+- user
+- uid
+- version
+- lat
+- lon
+- timestamp
+- changeset
+All other attributes can be ignored
+
+The "node_tags" field should hold a list of dictionaries, one per secondary tag. Secondary tags are
+child tags of node which have the tag name/type: "tag". Each dictionary should have the following
+fields from the secondary tag attributes:
+- id: the top level node id attribute value
+- key: the full tag "k" attribute value if no colon is present or the characters after the colon if one is.
+- value: the tag "v" attribute value
+- type: either the characters before the colon in the tag "k" value or "regular" if a colon
+        is not present.
+
+Additionally,
+
+- if the tag "k" value contains problematic characters, the tag should be ignored
+- if the tag "k" value contains a ":" the characters before the ":" should be set as the tag type
+  and characters after the ":" should be set as the tag key
+- if there are additional ":" in the "k" value they and they should be ignored and kept as part of
+  the tag key. For example:
+
+  <tag k="addr:street:name" v="Lincoln"/>
+  should be turned into
+  {'id': 12345, 'key': 'street:name', 'value': 'Lincoln', 'type': 'addr'}
+
+- If a node has no secondary tags then the "node_tags" field should just contain an empty list.
+
+The final return value for a "node" element should look something like:
+
+{'node': {'id': 757860928,
+          'user': 'uboot',
+          'uid': 26299,
+       'version': '2',
+          'lat': 41.9747374,
+          'lon': -87.6920102,
+          'timestamp': '2010-07-22T16:16:51Z',
+      'changeset': 5288876},
+ 'node_tags': [{'id': 757860928,
+                'key': 'amenity',
+                'value': 'fast_food',
+                'type': 'regular'},
+               {'id': 757860928,
+                'key': 'cuisine',
+                'value': 'sausage',
+                'type': 'regular'},
+               {'id': 757860928,
+                'key': 'name',
+                'value': "Shelly's Tasty Freeze",
+                'type': 'regular'}]}
+
+### If the element top level tag is "way":
+The dictionary should have the format {"way": ..., "way_tags": ..., "way_nodes": ...}
+
+The "way" field should hold a dictionary of the following top level way attributes:
+- id
+- user
+- uid
+- version
+- timestamp
+- changeset
+
+All other attributes can be ignored
+
+The "way_tags" field should again hold a list of dictionaries, following the exact same rules as
+for "node_tags".
+
+Additionally, the dictionary should have a field "way_nodes". "way_nodes" should hold a list of
+dictionaries, one for each nd child tag.  Each dictionary should have the fields:
+- id: the top level element (way) id
+- node_id: the ref attribute value of the nd tag
+- position: the index starting at 0 of the nd tag i.e. what order the nd tag appears within
+            the way element
+
+The final return value for a "way" element should look something like:
+
+{'way': {'id': 209809850,
+         'user': 'chicago-buildings',
+         'uid': 674454,
+         'version': '1',
+         'timestamp': '2013-03-13T15:58:04Z',
+         'changeset': 15353317},
+ 'way_nodes': [{'id': 209809850, 'node_id': 2199822281, 'position': 0},
+               {'id': 209809850, 'node_id': 2199822390, 'position': 1},
+               {'id': 209809850, 'node_id': 2199822392, 'position': 2},
+               {'id': 209809850, 'node_id': 2199822369, 'position': 3},
+               {'id': 209809850, 'node_id': 2199822370, 'position': 4},
+               {'id': 209809850, 'node_id': 2199822284, 'position': 5},
+               {'id': 209809850, 'node_id': 2199822281, 'position': 6}],
+ 'way_tags': [{'id': 209809850,
+               'key': 'housenumber',
+               'type': 'addr',
+               'value': '1412'},
+              {'id': 209809850,
+               'key': 'street',
+               'type': 'addr',
+               'value': 'West Lexington St.'},
+              {'id': 209809850,
+               'key': 'street:name',
+               'type': 'addr',
+               'value': 'Lexington'},
+              {'id': '209809850',
+               'key': 'street:prefix',
+               'type': 'addr',
+               'value': 'West'},
+              {'id': 209809850,
+               'key': 'street:type',
+               'type': 'addr',
+               'value': 'Street'},
+              {'id': 209809850,
+               'key': 'building',
+               'type': 'regular',
+               'value': 'yes'},
+              {'id': 209809850,
+               'key': 'levels',
+               'type': 'building',
+               'value': '1'},
+              {'id': 209809850,
+               'key': 'building_id',
+               'type': 'chicago',
+               'value': '366409'}]}
+"""
+
+import csv
+import codecs
+import re
+import xml.etree.ElementTree as ET
+
+import cerberus
+
+import schema
+
+from audit import update_name, is_street_name
+
+OSM_PATH = "D:\\chengdu_china\\chengdu_china.osm"
+
+NODES_PATH = "D:\\chengdu_china\\nodes.csv"
+NODE_TAGS_PATH = "D:\\chengdu_china\\nodes_tags.csv"
+WAYS_PATH = "D:\\chengdu_china\\ways.csv"
+WAY_NODES_PATH = "D:\\chengdu_china\\ways_nodes.csv"
+WAY_TAGS_PATH = "D:\\chengdu_china\\ways_tags.csv"
+
+LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
+PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+PHONENUM = re.compile(r'\+86\s\d{3}\s\d{4}\s\d{4}')
+POSTCODE = re.compile(r'\d{6}')
+
+SCHEMA = schema.schema
+
+# Make sure the fields order in the csvs matches the column order in the sql table schema
+NODE_FIELDS = ['id', 'lat', 'lon', 'user', 'uid', 'version', 'changeset', 'timestamp']
+NODE_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
+WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_NODES_FIELDS = ['id', 'node_id', 'position']
+
+
+def update_city(city):
+    if city in["成都","成都市","chengdu","Chengu"]:
+        city = "Chengdu"
+    return city
+def update_postcode(postcode):
+    m = POSTCODE.match(postcode)
+    if m is not None:
+        return postcode
+    else:
+        # Keep zip code revealed in postal code audit for document deletion purposes
+        if postcode == "028":
+            postcode = "610041"
+            return postcode   
+        # Ignore tag if improper postal code format
+        else:
+            return None
+    
+def update_phone_num(phone_num):
+    """
+    Clean phone number for insertion into SQL database
+    """
+    # Check for valid phone number format
+    m = PHONENUM.match(phone_num)
+    if m is None:
+        # Convert all dashes to spaces
+        if "-" in phone_num:
+            phone_num = re.sub("-", "", phone_num)
+        # Remove all brackets
+        if "(" in phone_num or ")" in phone_num:
+            phone_num = re.sub("[()]", "", phone_num)
+
+        # Remove the first two zero
+        if re.match(r'^0{2}', phone_num) is not None:
+            phone_num = re.sub("^0{2}", "", phone_num)
+            
+        # Remove all the spaces        
+        if " " in phone_num:
+            phone_num = re.sub(" ", "", phone_num)  
+            
+        # Add full country code and spaces   
+        length = len(phone_num)
+        if re.match(r'^1\d{10}', phone_num) is not None:
+            phone_num ="+86 "+phone_num      
+        elif length>=8 and length<=14:
+            phone_num ="+86 028 "+phone_num[length-8:length-4]+" "+phone_num[length-4:]
+       
+
+        # Ignore tag if no area code and local number (<10 digits)
+        elif sum(c.isdigit() for c in phone_num) < 10:
+            return None
+    return phone_num
+
+
+def load_new_tag(element, secondary, default_tag_type):
+    """
+    Load a new tag dict to go into the list of dicts for way_tags, node_tags
+    """
+    new = {}
+    new['id'] = element.attrib['id']
+    if ":" not in secondary.attrib['k']:
+        new['key'] = secondary.attrib['k']
+        new['type'] = default_tag_type
+    else:
+        post_colon = secondary.attrib['k'].index(":") + 1
+        new['key'] = secondary.attrib['k'][post_colon:]
+        new['type'] = secondary.attrib['k'][:post_colon - 1]
+
+    # Cleaning and loading values of various keys
+    if is_street_name(secondary):
+        # Why don't i need to use mapping, street_mapping,
+        # and num_line_mapping dicts  as params?
+        street_name = update_name(secondary.attrib['v'])
+        new['value'] = street_name
+    
+    elif new['key'] == 'phone':
+        phone_num = update_phone_num(secondary.attrib['v'])
+        if phone_num is not None:
+            new['value'] = phone_num
+        else:
+            return None
+    elif new['key'] == 'city':
+        city = update_city(secondary.attrib['v'])
+        new['value'] = city
+#    elif new['key'] == 'housenumber':
+#        housenumber = update_housenumber(secondary.attrib['v'])
+#        new['value'] = housenumber
+#    elif new['key'] == 'province':
+#        # Change Ontario to ON
+#        province = secondary.attrib['v']
+#        if province == 'Ontario':
+#            province = 'ON'
+#        new['value'] = province
+#
+    elif new['key'] == 'postcode':
+        post_code = update_postcode(secondary.attrib['v'])        
+        new['value'] = post_code
+        
+
+    else:
+        new['value'] = secondary.attrib['v']
+    
+    return new
+
+
+def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS,
+                  problem_chars=PROBLEMCHARS, default_tag_type='regular'):
+    """Clean and shape node or way XML element to Python dict"""
+
+    node_attribs = {}
+    way_attribs = {}
+    way_nodes = []
+    tags = []  # Handle secondary tags the same way for both node and way elements
+
+    if element.tag == 'node':
+        for attrib, value in element.items():
+            if attrib in node_attr_fields:
+                node_attribs[attrib] = value
+        
+        # For elements within the top element
+        for secondary in element.iter():
+            if secondary.tag == 'tag':
+                if problem_chars.match(secondary.attrib['k']) is not None:
+                    continue
+                else:
+                    new = load_new_tag(element, secondary, default_tag_type)
+                    if new is not None:
+                        tags.append(new)
+        return {'node': node_attribs, 'node_tags': tags}
+    
+    elif element.tag == 'way':
+        for attrib, value in element.items():
+            if attrib in way_attr_fields:
+                way_attribs[attrib] = value
+
+        counter = 0
+        for secondary in element.iter():
+            if secondary.tag == 'tag':
+                if problem_chars.match(secondary.attrib['k']) is not None:
+                    continue
+                else:
+                    new = load_new_tag(element, secondary, default_tag_type)
+                    if new is not None:
+                        tags.append(new)
+            elif secondary.tag == 'nd':
+                newnd = {}
+                newnd['id'] = element.attrib['id']
+                newnd['node_id'] = secondary.attrib['ref']
+                newnd['position'] = counter
+                counter += 1
+                way_nodes.append(newnd)
+        
+        return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
+
+
+# ================================================== #
+#               Helper Functions                     #
+# ================================================== #
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag"""
+
+    context = ET.iterparse(osm_file, events=('start', 'end'))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+
+def validate_element(element, validator, schema=SCHEMA):
+    """Raise ValidationError if element does not match schema"""
+    if validator.validate(element, schema) is not True:
+        field, errors = next(validator.errors.items())
+        message_string = "\nElement of type '{0}' has the following errors:\n{1}"
+        error_strings = (
+            "{0}: {1}".format(k, v if isinstance(v, str) else ", ".join(v))
+            for k, v in errors.items()
+        )
+        raise cerberus.ValidationError(
+            message_string.format(field, "\n".join(error_strings))
+        )
+
+
+class UnicodeDictWriter(csv.DictWriter, object):
+    """Extend csv.DictWriter to handle Unicode input"""
+
+    def writerow(self, row):
+        super(UnicodeDictWriter, self).writerow({
+            k: v for k, v in row.items()
+        })
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+# ================================================== #
+#               Main Function                        #
+# ================================================== #
+def process_map(file_in, validate):
+    """Iteratively process each XML element and write to csv(s)"""
+
+    with codecs.open(NODES_PATH, 'w','utf-8') as nodes_file, \
+         codecs.open(NODE_TAGS_PATH, 'w','utf-8') as nodes_tags_file, \
+         codecs.open(WAYS_PATH, 'w','utf-8') as ways_file, \
+         codecs.open(WAY_NODES_PATH, 'w','utf-8') as way_nodes_file, \
+         codecs.open(WAY_TAGS_PATH, 'w','utf-8') as way_tags_file:
+
+        nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
+        node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
+        ways_writer = UnicodeDictWriter(ways_file, WAY_FIELDS)
+        way_nodes_writer = UnicodeDictWriter(way_nodes_file, WAY_NODES_FIELDS)
+        way_tags_writer = UnicodeDictWriter(way_tags_file, WAY_TAGS_FIELDS)
+
+        nodes_writer.writeheader()
+        node_tags_writer.writeheader()
+        ways_writer.writeheader()
+        way_nodes_writer.writeheader()
+        way_tags_writer.writeheader()
+
+        validator = cerberus.Validator()
+
+        for element in get_element(file_in, tags=('node', 'way')):
+            el = shape_element(element)
+            if el:
+                if validate is True:
+                    validate_element(el, validator)
+
+                if element.tag == 'node':
+                    nodes_writer.writerow(el['node'])
+                    node_tags_writer.writerows(el['node_tags'])
+                elif element.tag == 'way':
+                    ways_writer.writerow(el['way'])
+                    way_nodes_writer.writerows(el['way_nodes'])
+                    way_tags_writer.writerows(el['way_tags'])
+
+
+if __name__ == '__main__':
+    # Note: Validation is ~ 10X slower. For the project consider using a small
+    # sample of the map when validating.
+    process_map(OSM_PATH, validate=True)
